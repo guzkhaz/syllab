7843b4fd1509332356a3d12c32cb604945163bb1
Guzel <guzkhaz04@gmail.com>
2024-09-29 23:07:51 +0300

    первый

---
+++ scripts/1.js
@@ -0,0 +1,46 @@
+const consonants = ['Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ'];
+
+const vowels = ['А', 'Е', 'Ё', 'И', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я'];
+
+let consonantIndex = 0;
+let vowelIndex = 0;
+
+    function displaySyllable() {
+    const syllable = consonants[consonantIndex] + vowels[vowelIndex];
+    document.getElementById("syllable").innerText = syllable;
+    document.getElementById("consonant").innerText = consonants[consonantIndex];
+    document.getElementById("vowel").innerText = vowels[vowelIndex];
+}
+
+    // Обработчик для кнопки "Согласная вверх"
+    document.getElementById("consonantUp").addEventListener("click", function() {
+    if (consonantIndex > 0 ) {
+    consonantIndex--;
+}
+    document.getElementById("consonant").innerText = consonants[consonantIndex];
+});
+
+    // Обработчик для кнопки "Согласная вниз"
+    document.getElementById("consonantDown").addEventListener("click", function() {
+    if (consonantIndex < consonants.length - 1) {
+    consonantIndex++;
+}
+    document.getElementById("consonant").innerText = consonants[consonantIndex];
+});
+
+    // Обработчик для кнопки "Гласная вверх"
+    document.getElementById("vowelUp").addEventListener("click", function() {
+    if (vowelIndex > 0) {
+    vowelIndex--;
+}
+    document.getElementById("vowel").innerText = vowels[vowelIndex];
+});
+
+    // Обработчик для кнопки "Гласная вниз"
+    document.getElementById("vowelDown").addEventListener("click", function() {
+    if (vowelIndex < vowels.length - 1) {
+    vowelIndex++;
+}
+    document.getElementById("vowel").innerText = vowels[vowelIndex];
+});
+
